4bb879edc4eb653e0e1cd0a9f495ecd453b1aac9
PaulMouly <paul.mouly100@gmail.com>
2024-07-08 17:03:21 +0200

    API creation

---
+++ API/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+import numpy as np
+from xgboost import XGBClassifier
+
+app = Flask(__name__)
+
+# Chargement du modèle pré-entraîné
+model = joblib.load('model/xgboost_model.pkl')  
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    df = pd.DataFrame(data)
+    prediction = model.predict(df)
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
