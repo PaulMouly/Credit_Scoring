8c4dd181b914ce8b34a89e521a66936f2cbb96f4
Paul MOULY <paul.mouly100@gmail.com>
2024-07-15 11:43:34 +0200

    Update

--- API/app.py
+++ API/app.py
@@ -1,8 +1,6 @@
 
 from flask import Flask, request, jsonify
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import OrdinalEncoder
 from xgboost import XGBClassifier
 import joblib
 import os
@@ -23,12 +21,12 @@ logger = logging.getLogger(__name__)
 model_path = 'C:/Users/paulm/Documents/Projet 7/Projet7withCSV/model/xgboost_model.pkl'
 model = joblib.load(model_path)
 
+# Chargement des données prétraitées depuis le CSV
+processed_data_path = 'C:/Users/paulm/Documents/Projet 7/Projet7withCSV/data/X_prediction.csv'
+df_prediction = pd.read_csv(processed_data_path)
+
 # Extraction des noms de colonnes utilisées pour l'entraînement
 cols_when_model_builds = model.get_booster().feature_names
-
-# Chargement des données originales pour la correspondance SK_ID_CURR
-chemin_dossier = "C:/Users/paulm/Documents/Projet 7/Projet7withCSV/data/"
-df_original = pd.read_csv(os.path.join(chemin_dossier, 'processed_data.csv'))
 
 @app.route('/predict', methods=['GET'])
 def predict():
@@ -41,29 +39,17 @@ def predict():
 
         logger.info(f"SK_ID_CURR reçu : {sk_id_curr}")
 
-        data_row = df_original[df_original['SK_ID_CURR'] == int(sk_id_curr)]
+        # Récupérer les données correspondant à SK_ID_CURR depuis df_prediction
+        data_row = df_prediction[df_prediction['SK_ID_CURR'] == int(sk_id_curr)]
 
         if data_row.empty:
             logger.warning(f"Aucune donnée trouvée pour SK_ID_CURR {sk_id_curr}")
             return jsonify({'error': f'Aucune donnée trouvée pour SK_ID_CURR {sk_id_curr}.'}), 404
 
         df = data_row.copy()
-        df.drop(columns=['ORGANIZATION_TYPE', 'OCCUPATION_TYPE'], inplace=True)
-        df.replace([np.inf, -np.inf], np.nan, inplace=True)
-
-        ordinal_encoder = OrdinalEncoder()
-        categorical_cols = df.select_dtypes(include='object').columns
-        df[categorical_cols] = ordinal_encoder.fit_transform(df[categorical_cols])
-
-        df = df[cols_when_model_builds]  # Réorganiser les colonnes selon l'ordre attendu
 
         # Vérifiez la forme des données avant la prédiction
         logger.info(f"Shape des données avant prédiction : {df.shape}")
-
-        # Vérifiez et gérez les valeurs manquantes si nécessaire
-        if df.isnull().values.any():
-            logger.warning("Il y a des valeurs manquantes dans les données à prédire.")
-            df.fillna(0, inplace=True)  # Par exemple, remplissez les valeurs manquantes avec zéro
 
         X = df.drop(columns=['SK_ID_CURR'])
         X_np = X.values  # Convertir en matrice NumPy
