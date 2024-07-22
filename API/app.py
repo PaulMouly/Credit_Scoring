f34508b7b50a748e51452a8d5f4f38b6f477dfbf
Paul MOULY <paul.mouly100@gmail.com>
2024-07-22 10:19:44 +0200

    test change

--- API/app.py
+++ API/app.py
@@ -84,3 +84,5 @@ def predict():
         logger.error(f"Erreur lors de la prédiction : {e}")
         return jsonify({'error': str(e)}), 400
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5010) 
