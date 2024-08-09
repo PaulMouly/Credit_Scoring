4f8a7af43e30fc97729e2eed4cd12fc556965feb
Paul MOULY <paul.mouly100@gmail.com>
2024-08-09 12:01:04 +0200

    add init

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,12 @@ import pytest
 # Ajouter le répertoire 'API' au sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'API')))
 
-import app
+from API import app
 
 @pytest.fixture
 def client():
     app.app.config['TESTING'] = True
     with app.app.test_client() as client:
         yield client
+
 
